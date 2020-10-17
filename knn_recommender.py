a81055d2a282568b9e1a1d3172ed351643e21ebc
sainioan <sainio.anniina@gmail.com>
2020-10-17 20:38:47 +0300

    search

--- knn_recommender.py
+++ knn_recommender.py
@@ -1,11 +1,12 @@
 import matplotlib
 import pandas as pd
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import numpy as np
 from jedi.api.refactoring import inline
 from sklearn.neighbors import NearestNeighbors
 from ast import literal_eval
 import string
+
 # from PIL import Image
 # import PIL.ImageOps
 # import matplotlib.pyplot as plt
@@ -13,18 +14,33 @@ import string
 # from wordcloud import WordCloud
 
 
-#%%
+# %%
 app = Flask(__name__)
-#%%
+
+
+# %%
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "GET":
+        return render_template("search.html")
+    if request.method == "POST":
+        name= str(request.form.get("name"))
+# name = input('Enter song title: ')
+    return name
+
+
 @app.route("/")
 def index():
     return render_template("home.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 df = pd.read_csv("songs_w_genres_v4.csv")
 df.head()
 
-#%%
+# %%
 
 # im = Image.open("guitar.png")
 # mask = np.array(im)
@@ -44,58 +60,61 @@ df.head()
 # plt.show()
 
 
+# %%
 
-#%%
-
-df_data = df.drop(['artists', 'duration_ms', 'explicit', 'mode', 'name', 'popularity', 'release_date', 'year', 'genres', 'standardized_name'], axis=1)
+df_data = df.drop(['artists', 'duration_ms', 'explicit', 'mode', 'name', 'popularity', 'release_date', 'year', 'genres',
+                   'standardized_name'], axis=1)
 df_data.index = df_data['id']
 df_data = df_data.drop(['id'], axis=1)
 df_data.head()
 
-#%%
+# %%
 
-df_data['key'] = df_data['key']/12
+df_data['key'] = df_data['key'] / 12
 max_loudness = df_data['loudness'].max()
-df_data['loudness'] = abs(df_data['loudness']/max_loudness)
+df_data['loudness'] = abs(df_data['loudness'] / max_loudness)
 max_tempo = df_data['tempo'].max()
-df_data['tempo'] = df_data['tempo']/max_tempo
+df_data['tempo'] = df_data['tempo'] / max_tempo
 df_data.head()
 
-
-#%%
+# %%
 
 knn = NearestNeighbors(n_neighbors=50, algorithm='ball_tree')
 knn.fit(df_data.values)
 
-#%%
+
+# %%
 
 def standardize(s):
     s = "".join([i.lower() for i in s if i not in frozenset(string.punctuation)])
     return s
 
-#%%
+
+# %%
 
 def matching_genres(genres1, genres2):
     genres1 = literal_eval(genres1)
     matches = list(set(genres1) & set(genres2))
     return len(matches) > 0
 
-#%%
+
+# %%
 
 neighbors = 25000
 
-max_duration = 360 # minutes
-max_duration *= 60000 # to milliseconds
+max_duration = 360  # minutes
+max_duration *= 60000  # to milliseconds
 
-#%%
+
+# %%
 
 def recommend(id, model, duration):
-    query = df_data.loc[id].to_numpy().reshape(1,-1)
-    distances, indices = model.kneighbors(query,n_neighbors = neighbors)
+    query = df_data.loc[id].to_numpy().reshape(1, -1)
+    distances, indices = model.kneighbors(query, n_neighbors=neighbors)
 
-    songs = df.loc[indices[0]].where(df['id']!=id).dropna()
+    songs = df.loc[indices[0]].where(df['id'] != id).dropna()
 
-    songs = songs.drop_duplicates(subset=['name','artists'])
+    songs = songs.drop_duplicates(subset=['name', 'artists'])
 
     searched = df.where(df['id'] == id).dropna().head()
     genres = literal_eval(searched['genres'].values[0])
@@ -103,7 +122,7 @@ def recommend(id, model, duration):
     if len(genres) > 0:
         songs = songs[songs['genres'].apply(matching_genres, args=([genres]))]
 
-    duration *= 60000 # convert to milliseconds
+    duration *= 60000  # convert to milliseconds
     duration = duration if duration < max_duration else max_duration
 
     count = songs[songs['duration_ms'].cumsum() < duration]['id'].count()
@@ -113,7 +132,8 @@ def recommend(id, model, duration):
 
     return songs
 
-#%%
+
+# %%
 
 def index_input():
     good_input = 0
@@ -129,7 +149,8 @@ def index_input():
         good_input = 1
     return ind
 
-#%%
+
+# %%
 
 def duration_input():
     duration = input('Enter duration of playlist in minutes: ')
@@ -137,18 +158,21 @@ def duration_input():
         duration = input('Duration has to be a number (Leave out the word minutes). Please try again: ')
     return duration
 
-#%%
 
-name = input('Enter song title: ')
+# %%
+
+name = search()
 name = standardize(name)
 
 duration = int(duration_input())
 
-found = df[['name', 'artists', 'popularity']].where(df['standardized_name'].str.contains(name)).dropna().sort_values(ascending=False, by="popularity")
+found = df[['name', 'artists', 'popularity']].where(df['standardized_name'].str.contains(name)).dropna().sort_values(
+    ascending=False, by="popularity")
 while found.empty:
     name = input('No songs found with that title. Enter a new song title.')
     name = standardize(name)
-    found = df[['name', 'artists', 'popularity']].where(df['standardized_name'].str.contains(name)).dropna().sort_values(ascending=False, by="popularity")
+    found = df[['name', 'artists', 'popularity']].where(
+        df['standardized_name'].str.contains(name)).dropna().sort_values(ascending=False, by="popularity")
 found = found[['name', 'artists']]
 print('Search results: \n', found)
 
@@ -162,33 +186,13 @@ print('\nSearching for', song, 'by', artists)
 
 songs = recommend(id, knn, duration)
 
-#%%
+# %%
 
 searched = df.where(df['id'] == id).dropna().head()
 
-pd.concat([searched,songs]).head()
+pd.concat([searched, songs]).head()
 
-
-#%%
-
-
-
+# %%
 
 
 # again empty space
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
